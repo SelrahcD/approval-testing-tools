6ec75c343afccabaf6188316de96fd2e7642687b
SelrahcD <c.desneuf@gmail.com>
2023-11-02 20:01:07 +0100

    Add UUUID Scrubber

--- src/Scrubbers/UuidScrubber/UuidScrubberTest.php
+++ src/Scrubbers/UuidScrubber/UuidScrubberTest.php
@@ -5,29 +5,49 @@ declare(strict_types=1);
 namespace Selrahcd\ApprovalTestingTools\Scrubbers\UuidScrubber;
 
 use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 
 class UuidScrubberTest extends TestCase
 {
+
+    use MatchesSnapshots;
 
     /**
      * @test
      */
     public function removes_uuids_from_a_string(): void
     {
+        // tag::removes_uuids_from_string_code[]
         $inputString = <<<EOS
 Here is a 5c2068a8-7213-42f6-81d7-ef3a270d7bd1,
 and here is another one 52276882-9853-4e3d-9e8d-c54c77db03a4
 EOS;
 
-        $uuidScrubber = new UuidScrubber;
+        $uuidScrubber = new UuidScrubber();
 
-        $actualScrubbedText = $uuidScrubber->scrub($inputString);
+        $scrubbedText = $uuidScrubber->scrub($inputString);
+        // end::removes_uuids_from_string_code[]
 
-        $expectedScrubbedText = <<<EOS
-Here is a UUID_0,
-and here is another one UUID_1
+        $this->assertMatchesSnapshot($scrubbedText);
+    }
+
+    /**
+     * @test
+     */
+    public function reuses_id_for_a_uuid_found_multiple_times(): void
+    {
+        // tag::reuses_id_for_a_uuid_found_multiple_times[]
+        $inputString = <<<EOS
+Here is a 5c2068a8-7213-42f6-81d7-ef3a270d7bd1,
+and here is another one 52276882-9853-4e3d-9e8d-c54c77db03a4,
+and here is the first one again 5c2068a8-7213-42f6-81d7-ef3a270d7bd1
 EOS;
-;
-        $this->assertEquals($expectedScrubbedText, $actualScrubbedText);
+
+        $uuidScrubber = new UuidScrubber();
+
+        $scrubbedText = $uuidScrubber->scrub($inputString);
+        // end::reuses_id_for_a_uuid_found_multiple_times[]
+
+        $this->assertMatchesSnapshot($scrubbedText);
     }
 }
